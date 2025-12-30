7fcb3de0d7ae09e7b63c9f5d3b9b136c428ab408
Tim Byrne <sultan@locehilios.com>
2025-12-29 23:06:54 -0600

    WIP

--- sketches/thermdet/thermdet.ino
+++ sketches/thermdet/thermdet.ino
@@ -1,14 +1,14 @@
-#include <Adafruit_Soundboard.h>
-
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(3, INPUT_PULLUP);
 }
 
 void loop() {
-  Serial.print("I'm alive\n");
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay((10 * 1000)-500);
+  if (digitalRead(3) == HIGH) {
+    Serial.print("HIGH\n");
+  }
+  else {
+    Serial.print("LOW\n");
+  }
+  delay(250);
 }
