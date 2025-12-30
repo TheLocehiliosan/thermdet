1d97ac10fdd5a31c8f4a89c766dc3b6d82978f02
Tim Byrne <sultan@locehilios.com>
2025-12-30 00:04:16 -0600

    WIP

--- sketches/thermdet/thermdet.ino
+++ sketches/thermdet/thermdet.ino
@@ -1,14 +1,60 @@
+// PINS
+// avoid 0 & 1 to preserve serial communication
+// 5 is special on the ItsyBitsy (high voltage output only)
+#define PIN_LED_TOP      A0
+#define PIN_LED_LEFT     A1
+#define PIN_LED_MIDDLE   A2
+#define PIN_LED_RIGHT    A3
+#define PIN_TRIGGER       3
+#define PIN_SND_RST       7
+#define PIN_SND_ACTIVITY  9
+#define PIN_SND_ARM      10
+#define PIN_SND_LOOP     11
+#define PIN_SND_DISARM   12
+
+const int ledPins[] = {PIN_LED_TOP, PIN_LED_LEFT, PIN_LED_MIDDLE, PIN_LED_RIGHT};
+const int sndPins[] = {PIN_SND_ARM, PIN_SND_LOOP, PIN_SND_DISARM};
+
+enum triggerState {armed, disarmed};
+enum triggerState last_tstate = disarmed;
+enum triggerState this_tstate = disarmed;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(3, INPUT_PULLUP);
+  // set up LEDs
+  for (int pin: ledPins) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
+  // set up sound
+  pinMode(PIN_SND_RST, OUTPUT);
+  digitalWrite(PIN_SND_RST, LOW);
+  pinMode(PIN_SND_ACTIVITY, INPUT_PULLUP);
+  for (int pin: sndPins) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+  }
+  digitalWrite(PIN_SND_RST, HIGH);
+
+  // set up trigger
+  pinMode(PIN_TRIGGER, INPUT_PULLUP);
 }
 
 void loop() {
-  if (digitalRead(3) == HIGH) {
-    Serial.print("HIGH\n");
+  if (digitalRead(PIN_TRIGGER) == HIGH) {
+    this_tstate = disarmed;
   }
   else {
-    Serial.print("LOW\n");
+    this_tstate = armed;
   }
-  delay(250);
+  if (this_tstate != last_tstate) {
+    if (this_tstate == armed) {
+      Serial.print("Armed\n");
+    }
+    else {
+      Serial.print("Disarmed\n");
+    }
+  }
+  delay(10);
+  last_tstate = this_tstate;
 }
