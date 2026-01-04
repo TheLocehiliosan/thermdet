b9f29b667cf9bab69b5d6b9ea98f716f787e8b79
Tim Byrne <sultan@locehilios.com>
2026-01-04 15:53:59 -0600

    WIP

--- sketches/thermdet/thermdet.ino
+++ sketches/thermdet/thermdet.ino
@@ -1,3 +1,11 @@
+#include <ALib0.h>
+
+/* Construction Notes:
+     Unused pins:
+      * PIN_SND_RST
+      * PIN_SND_ACTIVITY
+*/
+
 // PINS
 // avoid 0 & 1 to preserve serial communication
 // 5 is special on the ItsyBitsy (high voltage output only)
@@ -12,20 +20,24 @@
 #define PIN_SND_LOOP     11
 #define PIN_SND_DISARM   12
 
+AButton trigger(PIN_TRIGGER);
+
 const int ledPins[] = {PIN_LED_TOP, PIN_LED_LEFT, PIN_LED_MIDDLE, PIN_LED_RIGHT};
 const int sndPins[] = {PIN_SND_ARM, PIN_SND_LOOP, PIN_SND_DISARM};
 
-enum triggerState {armed, disarmed};
-enum triggerState last_tstate = disarmed;
-enum triggerState this_tstate = disarmed;
+boolean armed = false;
 
 void setup() {
+  // set up debugging serial port
   Serial.begin(9600);
+  //while (!Serial) { delay(1); }
+
   // set up LEDs
   for (int pin: ledPins) {
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
+
   // set up sound
   pinMode(PIN_SND_RST, OUTPUT);
   digitalWrite(PIN_SND_RST, LOW);
@@ -38,23 +50,87 @@ void setup() {
 
   // set up trigger
   pinMode(PIN_TRIGGER, INPUT_PULLUP);
+  armed = digitalRead(PIN_TRIGGER);
+
+  Serial.println("Setup complete");
+}
+
+void watch_trigger() {
+  taskBegin();
+  if (trigger.changed()) {
+    armed = digitalRead(PIN_TRIGGER);
+    Serial.print("armed changed to ");
+    Serial.println(armed, HEX);
+  }
+  taskEnd();
+}
+
+void make_noise() {
+  taskBegin();
+  taskWaitFor(armed);
+  digitalWrite(PIN_SND_ARM, LOW); taskDelay(300); digitalWrite(PIN_SND_ARM, HIGH);
+  digitalWrite(PIN_SND_LOOP, LOW);
+  taskWaitFor(!armed);
+  digitalWrite(PIN_SND_LOOP, HIGH);
+  digitalWrite(PIN_SND_DISARM, LOW); taskDelay(300); digitalWrite(PIN_SND_DISARM, HIGH);
+  taskEnd();
+}
+
+void counter() {
+  static float speed = 1.0;
+  static int   count = 0;
+  taskBegin();
+  if (!armed) {
+    count = 0;
+    speed = 1.0;
+    digitalWrite(PIN_LED_LEFT,   LOW);
+    digitalWrite(PIN_LED_MIDDLE, LOW);
+    taskRestart();
+  }
+  count++;
+  if (count > 3) {
+    count = 1;
+  }
+  digitalWrite(PIN_LED_LEFT,   bitRead(count, 0) == 1);
+  digitalWrite(PIN_LED_MIDDLE, bitRead(count, 1) == 1);
+  taskDelay(1000 * (1.0/speed));
+  digitalWrite(PIN_LED_LEFT,   LOW);
+  digitalWrite(PIN_LED_MIDDLE, LOW);
+  if (speed <= 7.0) {
+    speed += 0.1;
+  }
+  taskEnd();
+}
+
+void warning_light() {
+  static float speed = 1.0;
+  taskBegin();
+  if (!armed) {
+    speed = 1.0;
+    digitalWrite(PIN_LED_RIGHT, LOW);
+    taskRestart();
+  }
+  taskDelay(3500 * (1.0/speed));
+  digitalWrite(PIN_LED_RIGHT, !digitalRead(PIN_LED_RIGHT));
+  speed += 0.5;
+  taskEnd();
+}
+
+void power_light() {
+  taskBegin();
+  if (!armed) {
+    digitalWrite(PIN_LED_TOP, LOW);
+    taskRestart();
+  }
+  digitalWrite(PIN_LED_TOP, random(100) > 50);
+  taskDelay(random(3000));
+  taskEnd();
 }
 
 void loop() {
-  if (digitalRead(PIN_TRIGGER) == HIGH) {
-    this_tstate = disarmed;
-  }
-  else {
-    this_tstate = armed;
-  }
-  if (this_tstate != last_tstate) {
-    if (this_tstate == armed) {
-      Serial.print("Armed\n");
-    }
-    else {
-      Serial.print("Disarmed\n");
-    }
-  }
-  delay(10);
-  last_tstate = this_tstate;
+  watch_trigger();
+  make_noise();
+  counter();
+  warning_light();
+  power_light();
 }
